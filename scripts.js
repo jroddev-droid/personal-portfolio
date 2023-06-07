d7c525e250d7108ff9d2b7f930ccc169cc7b0a3f
jroddev-droid <jroddev.droid@gmail.com>
2023-06-07 13:17:53 -0400

    Added form to contact page

--- scripts.js
+++ scripts.js
@@ -11,3 +11,18 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+function validateForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error = document.querySelector(".error");
+    
+    if (name == "" || email == "" || message == "") {
+      error.style.display = "block";
+      return false;
+    } else {
+      error.style.display = "none";
+      return true;
+    }
+  }
